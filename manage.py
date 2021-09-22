4ee4c8d92877841cd7ea4dab99a3dcb2035321e0
bensammwaniki <bensammwaniki@gmail.com>
2021-09-22 11:39:53 +0300

    added migration

--- manage.py
+++ manage.py
@@ -1,13 +1,20 @@
- 
-from app import create_app
-from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
+from app import create_app ,db
+from flask_script import Manager,Shell,Server
+from app.models import User,Role, Comment, Pitch
 
-
-# creating app instance
+# Creating app instance
 app = create_app('production')
-manager = Manager(app)
 
+
+manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,  Role = Role ,Comment = Comment, Pitch = Pitch  )
 
 
 if __name__ == '__main__':
