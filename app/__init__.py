c1232b19c729b127f5466c676763a16dbfc112e5
bensammwaniki <bensammwaniki@gmail.com>
2021-09-23 23:43:08 +0300

    reinitialized bluprintse

--- app/__init__.py
+++ app/__init__.py
@@ -19,11 +19,12 @@ def create_app(config_name):
 
     # Initializing flask extensions
     db.init_app(app)
-    bootstrap = Bootstrap(app)
+    # bootstrap = Bootstrap(app)
     login_manager.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
     # registering the main app blueprint
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
