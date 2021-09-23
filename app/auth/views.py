066e4e5199f35a87d9135671a414d21790d50aa1
bensammwaniki <bensammwaniki@gmail.com>
2021-09-24 00:15:58 +0300

    updated fkask uploads

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from .. import db
 from ..models import User  
 from flask_login import login_user, logout_user,  login_required
 from .form import RegistrationForm, LoginForm
-
+from ..email import mail_message
 @auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
